4087c02dde60e7ead9f7ab78b5738cc4078acacb
jakkrapon3236 <631320120@live.kpru.ac.th>
2023-08-26 00:04:11 +0700

    edit add page API varities

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
         Feature4: 2,
         Feature5: 1,
         Feature6: 1,
-        selectedVarity: 7,
+        selectedVarity: selectedVarity,
       })
       .then((res) => {
         console.log('Response status:', res.status);
