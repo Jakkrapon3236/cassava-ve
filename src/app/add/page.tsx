a07afbefcf052ea91dcdd1b30a37246694484e17
jakkrapon3236 <631320120@live.kpru.ac.th>
2023-09-04 15:38:35 +0700

    edit submit button\

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -429,12 +429,13 @@ export default function Home() {
                   </button>
                   <button
                     className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="submit" formAction={`https://cmdkpp.com/API/api_add_maping.php`}
+                    type="submit" formAction={`https://cmdkpp.com/API/api_add_maping.php`} formMethod="post" 
                     onClick={() => {
                       addData();
                   
                     }}
                   >
+                    
                     Save Changes
                   </button>
                 </div>
