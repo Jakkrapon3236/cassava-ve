0b6a666eb80c359e7369fb2abcb808e9e4d8c6a5
jakkrapon3236 <631320120@live.kpru.ac.th>
2023-09-06 23:09:48 +0700

    test succes 1

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -126,7 +126,7 @@ export default function Home() {
                   onClick={() => handleChange(`ft${ind}`, parseInt(`${i + 1}`))}
                   className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                 />
-                Radio Button Label
+                
               </label>
             </div>
             {description.length > 0 ? (
